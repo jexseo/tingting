f700ec8c55514b92d596bbd538da3e5cdbefb80d
JeXSEO <jexseokeras@gmail.com>
2025-06-11 11:45:55 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,4 +1,5 @@
 <div style="width:1px;height:1px;overflow:hidden">
 <p><a href="https://sig.tecnologicosucre.edu.ec/">https://sig.tecnologicosucre.edu.ec/</a></p>
-<p><a href="https://brystalpremium.com/">SLOT777</a></p>
+<p><a href="https://brystalpremium.com/">SLOT777</a></p> 
+<p><a href="https://jowaeltech.com/">https://jowaeltech.com/</a></p>
 </div>
