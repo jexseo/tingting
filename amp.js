79d85d64aa8bdd5520ee7cef7fbfd37d577b2df2
JeXSEO <jexseokeras@gmail.com>
2025-06-18 22:43:08 +0700

    Create amp.js

---
+++ amp.js
@@ -0,0 +1,220 @@
+<!doctype html>
+<html amp lang='id'>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='format-detection' content='telephone=no'>
+    <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1'>
+    <title>169CUAN RESMI > Agen Pusat Game Online Gacor Termurah Hari Ini </title>
+    <meta name='description' content='169CUAN ialah platform resmi agen pusatnya game online yang gacor hari ini dan semua jenis game 169cuan termasuk paling murah dapat dijangkau pemain. ' />
+    <meta name='keywords' content='169cuan, 169cuan slot, 169cuan agen, online gacor' />
+    <meta name='author' content='169CUAN' />
+    <meta name='language' content='id-ID' />
+    <meta name='robots' content='index, follow' />
+    <meta name='Slurp' content='all' />
+    <meta name='webcrawlers' content='all' />
+    <meta name='spiders' content='all' />
+    <meta name='allow-search' content='yes' />
+    <meta name='YahooSeeker' content='index,follow' />
+    <meta name='msnbot' content='index,follow' />
+    <meta name='expires' content='never' />
+    <meta name='rating' content='general'>
+    <meta name='publisher' content='169CUAN'>
+    <meta name='robots' content='index,follow' />
+    <meta name='googlebot' content='index,follow' />
+    <meta name='YahooSeeker' content='index,follow'>
+    <meta name='msnbot' content='index,follow'>
+    <link rel='canonical' href='https://eejournal.fei.tuke.sk/public/site/' />
+    <link rel='alternate' hreflang='id' href='https://eejournal.fei.tuke.sk/public/site/' />
+    <link rel='shortcut icon' type='image/x-icon' href='https://i.pinimg.com/736x/e6/a9/19/e6a919ddce3ace1b4011d3161730648b.jpg' />
+    <link rel='apple-touch-icon' href='https://i.pinimg.com/736x/e6/a9/19/e6a919ddce3ace1b4011d3161730648b.jpg' />
+    <link rel='icon' type='image/x-icon' href='https://i.pinimg.com/736x/e6/a9/19/e6a919ddce3ace1b4011d3161730648b.jpg' />
+    <script async src='https://cdn.ampproject.org/v0.js'></script>
+    <style amp-boilerplate>
+      body {
+        -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+      }
+      @-webkit-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-moz-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-ms-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-o-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+    </style>
+    <noscript>
+      <style amp-boilerplate>
+        body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}
+      </style>
+    </noscript>
+    <style amp-custom>
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        :focus {
+          outline: 0;
+        }
+        ::-webkit-scrollbar {
+          display: none;
+        }
+        a,
+        a:after,
+        a:hover,
+        a:visited {
+          text-decoration: none;
+          color: black;
+          font-weight: bold;
+        }
+        html {
+          max-width: 500px;
+          margin: 0 auto;
+          background: rgb(0, 0, 0);
+          display: grid;
+          min-height: 100vh;
+        }
+        body {
+          color: #000000;
+          font-family: 'Noto Sans', Arial, sans-serif;
+          display: grid;
+          min-height: 100vh;
+        }
+        /* Desktop background */
+        @media (min-width: 768px) {
+            body::before {
+                content: "";
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-image: url('https://i.ibb.co/fVM23N8m/169cuan-agen-pusat-game-online.png');
+                opacity: 0.7;
+                background-size: cover;
+                filter: blur(5px);
+                z-index: -1;
+                display: grid;
+                min-height: 100vh;
+            }
+        }
+
+        /* Mobile background */
+        @media (max-width: 767px) {
+            body::before {
+                content: "";
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-image: url('https://i.ibb.co/fVM23N8m/169cuan-agen-pusat-game-online.png');
+                opacity: 0.7;
+                background-size: cover;
+                filter: blur(5px);
+                z-index: -1;
+                display: grid;
+                min-height: 100vh;
+            }
+        }
+
+        .container {
+          display: grid;
+          min-height: 100vh;
+        }
+        .box {
+          margin: auto;
+          text-align: center;
+        }
+        .link-wrapper {
+          display: inline-grid;
+          margin: 0.88rem 0;
+        }
+        .link-wrapper a {
+          padding: 0.5rem 3.8rem;
+          background: rgb(0 0 0);
+          margin-bottom: 0.5rem;
+          border-radius: 0.38rem;
+          letter-spacing: 1px;
+          box-shadow: inset 0 0 0 3px #ffffff;
+          color: #ffffff;
+          font-weight: bold;
+        }
+        .link-wrapper a.btn1 {
+            color: #ffffff;
+            background: rgb(0 0 0);
+            box-shadow: inset 0 0 0 3px #ffffff;
+            font-weight: bold;
+        }
+        .imghero {
+          box-shadow: inset 0 0 0 5px #000;
+          border-radius: 8px;
+        }
+        main {
+            position: relative;
+            z-index: 1;
+        }
+
+        p {
+          color: #fff;
+          padding-left: 10px;
+          padding-right: 10px;
+        }
+    </style>
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="box">
+            <div>
+                <amp-img class="imghero" height="320" width="320" alt="Aplikasi Slot 169CUAN" src="https://i.ibb.co/fVM23N8m/169cuan-agen-pusat-game-online.png" layout="intrinsic"></amp-img>
+            </div>
+            <div class="link-wrapper">
+                <a href="https://awpgrup.link/hoki88/" class="btn1">LOGIN</a>
+                <a href="https://awpgrup.link/hoki88/" target="_blank" rel="noopener noreferrer nofollow">DAFTAR</a>
+            </div>
+            <p><a href='https://eejournal.fei.tuke.sk/public/site/' style="color:#fff">169CUAN</a> ialah platform resmi agen pusatnya game online yang gacor hari ini dan semua jenis game 169cuan termasuk paling murah dapat dijangkau pemain.</p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+
+
+
