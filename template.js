356474b6f57d0aff74a5c6f28ace713a326fcc48
JeXSEO <jexseokeras@gmail.com>
2025-06-28 06:01:15 +0700

    Update template.js

--- template.js
+++ template.js
@@ -6,5 +6,5 @@
 <p><a href="https://169cuanbro.glitch.me/">https://169cuanbro.glitch.me</a></p>
 <p><a href="https://eejournal.fei.tuke.sk/public/blog/">https://eejournal.fei.tuke.sk/public/blog/</a></p>
 <p><a href="https://eejournal.fei.tuke.sk/public/site/">169CUAN</a></p>
-<p><a href="https://dev.salakhospitality.com/"><Slot Pulsa</a></p>
+<p><a href="https://dev.salakhospitality.com/">Slot Pulsa</a></p>
 </div>
