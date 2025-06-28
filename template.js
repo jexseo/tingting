9bf183b73ad34552d9c5a035defff4ecd872a877
JeXSEO <jexseokeras@gmail.com>
2025-06-29 05:16:44 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,8 +1,4 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://sig.tecnologicosucre.edu.ec/">https://sig.tecnologicosucre.edu.ec/</a></p>
-<p><a href="https://brystalpremium.com/">SLOT777</a></p> 
-<p><a href="https://jowaeltech.com/">https://jowaeltech.com/</a></p>
-<p><a href="https://techwoly.com/">https://techwoly.com/</a></p>
 <p><a href="https://169cuanbro.glitch.me/">https://169cuanbro.glitch.me</a></p>
 <p><a href="https://eejournal.fei.tuke.sk/public/blog/">https://eejournal.fei.tuke.sk/public/blog/</a></p>
 <p><a href="https://eejournal.fei.tuke.sk/public/site/">169CUAN</a></p>
