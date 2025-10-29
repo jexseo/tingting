a3ce6557ad21ddd61dcc54dad9afa35b2aedbe22
JeXSEO <jexseokeras@gmail.com>
2025-10-29 13:30:02 +0700

    Update template.js

--- template.js
+++ template.js
@@ -28,7 +28,7 @@
 <p><a href="https://emrsrajouri.in/">RAJA88</a></p>
 <p><a href="https://bahta.desa.id/">https://bahta.desa.id/</a></p>
 <p><a href="https://simonas.langsakota.go.id/">https://simonas.langsakota.go.id/</a></p>
-
+<p><a href="https://pn-bangkalan.go.id">https://pn-bangkalan.go.id/</a></p>
 
   
   
