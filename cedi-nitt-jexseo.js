a564b22ac4f968e5bd2afe4f03f885ed6c8a34fe
JeXSEO <jexseokeras@gmail.com>
2026-01-09 07:55:54 +0700

    Update cedi-nitt-jexseo.js

--- cedi-nitt-jexseo.js
+++ cedi-nitt-jexseo.js
@@ -1689,6 +1689,7 @@
 <a href="https://cedi.nitt.edu/">nrtoto</a>
 <a href="https://cedi.nitt.edu/">nusa88</a>
 <a href="https://cedi.nitt.edu/">maxbet108</a>
+<a href="https://cedi.nitt.edu/">binar4d</a>
 <a href="https://cedi.nitt.edu/">jaminjp</a>
 <a href="https://cedi.nitt.edu/">ayubet</a>
 <a href="https://cedi.nitt.edu/">qqnusa21</a>
